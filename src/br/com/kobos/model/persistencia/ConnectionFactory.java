4f1608cd0619f25c0948f5c157795c27c9523238
guest01 <guest01@10.1.49.105>
2013-10-21 08:46:17 -0200

    Implementando Factory

---
+++ src/br/com/kobos/model/persistencia/ConnectionFactory.java
@@ -0,0 +1,44 @@
+
+package br.com.kobos.model.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    
+    private static final String STR_CONEXAO = "jdbc:mysql://localhost:3306/";
+    private static final String DATABASE = "kobosbd";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    
+    public static Connection getConnection() throws SQLException{
+        try{
+            return DriverManager.getConnection(STR_CONEXAO + DATABASE, USER, PASSWORD);
+        }catch (Exception e){
+            throw new SQLException(e.getMessage());
+        }
+    }
+    
+    public static void closeConnection(Connection conn) throws SQLException{
+        if (conn != null){
+            conn.close();
+        }
+    }
+    
+    public static void closeConnection(Connection conn, PreparedStatement pstm)throws SQLException{
+        if (pstm!= null){
+            pstm.close();
+        }
+        closeConnection(conn);
+    }
+    
+    public static void closeConnection(Connection conn, PreparedStatement pstm, ResultSet rs)throws SQLException{
+        if (rs != null){
+            rs.close();
+        }
+        closeConnection(conn, pstm);
+    }
+}
