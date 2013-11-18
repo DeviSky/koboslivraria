0f9aedde07a7e74f24ba3e86d955274e09298fa1
guest01 <guest01@10.1.49.105>
2013-11-18 08:39:10 -0200

    Concertado o login #1

--- src/br/com/kobos/app/Principal.java
+++ src/br/com/kobos/app/Principal.java
@@ -1,11 +1,11 @@
 
 package br.com.kobos.app;
 
-import br.com.kobos.view.JanelaLogin;
+import br.com.kobos.view.TelaLoginGUI;
 
 public class Principal {
     public static void main(String[] args){
-        JanelaLogin jl = new JanelaLogin();
+        TelaLoginGUI jl = new TelaLoginGUI();
         jl.setLocationRelativeTo(null);
         jl.setVisible(true);
     }
